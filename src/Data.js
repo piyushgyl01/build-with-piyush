ad649fc90d01679576a20d93d81a4596f607fe32
Piyush Goyal <piyush2022ug@gmail.com>
2024-12-21 22:26:42 +0530

    build: everything done except adding dev. projects

---
+++ src/Data.js
@@ -0,0 +1,139 @@
+export const projects = [
+    // Web Development and Mobile Development Projects
+    {
+      id: 1,
+      title: "Project One",
+      description: "Description One",
+      type: ["Web Development"],
+      image: "https://placehold.co/600x400/png",
+      websiteUrl: "https://project-one.com",
+      sourceCodeUrl: "https://github.com/username/project-one",
+      technologies: ["React", "Node.js", "MongoDB"],
+      featured: true,
+    },
+    {
+      id: 2,
+      title: "Mobile App Project",
+      description: "A cross-platform mobile application",
+      type: ["Mobile Development"],
+      image: "https://placehold.co/600x400/png",
+      websiteUrl: "https://mobile-project.com",
+      sourceCodeUrl: "https://github.com/username/mobile-project",
+      technologies: ["React Native", "Firebase"],
+      featured: false,
+    },
+    {
+      id: 3,
+      title: "Hybrid Project",
+      description: "Combined web and mobile project",
+      type: ["Web Development", "Mobile Development"],
+      image: "https://placehold.co/600x400/png",
+      websiteUrl: "https://hybrid-project.com",
+      sourceCodeUrl: "https://github.com/username/hybrid-project",
+      technologies: ["React", "React Native", "GraphQL"],
+      featured: false,
+    },
+  
+    // Product Design Projects
+    {
+      id: 4,
+      title: "Customer Support Page",
+      description: `"I designed a customer support and issue
+      tracking system to provide quick, clear answers to
+      users' product-related questions."`,
+      type: ["Product Design"],
+      image:
+        "https://media.discordapp.net/attachments/1156145270971715626/1320041212564213780/Frame_1000002483.png?ex=676827ae&is=6766d62e&hm=af43e5fde61146c0fa73ab098039a7cc4b47b623a6b1045153a63d41d04bb8c4&=&width=1050&height=700",
+      websiteUrl:
+        "https://www.figma.com/design/ex0UCTxbz3SNf0OkaeUG6I/Customer-Support-Page-By-Piyush-Goyal?node-id=0-1&t=ESdVthF52u2eR98C-1",
+      sourceCodeUrl: "https://github.com/username/design-system",
+      technologies: ["Web Design", "Figma"],
+      featured: true,
+    },
+    {
+      id: 5,
+      title: "Blog Page Design",
+      description: `"I designed and animated Design Studios' work
+      showcase page, starting with an eye-catching hero
+      section to immediately grab visitors' attention."`,
+      type: ["Product Design"],
+      image:
+        "https://media.discordapp.net/attachments/1156145270971715626/1320041451144613909/Frame_1000002489.png?ex=676827e7&is=6766d667&hm=023e8bcef83a43aff096dcea706e603a04ba84e7f4974788e47ca1696911cd27&=&width=1200&height=800",
+      websiteUrl:
+        "https://www.figma.com/design/dgubJXu8P1gKA8vu3fklpa/Blog-Page-by-Piyush-Goyal?node-id=0-1&t=ESdVthF52u2eR98C-1",
+      sourceCodeUrl: "https://github.com/username/design-system",
+      technologies: ["Web Design", "Figma"],
+      featured: true,
+    },
+    {
+      id: 6,
+      title: "Pocket Pod",
+      description: `"I redesigned PocketPod's user
+      onboarding flow to simplify AI podcast
+      generation."`,
+      type: ["Product Design"],
+      image:
+        "https://media.discordapp.net/attachments/1156145270971715626/1320041450821521408/Frame_1000002488.png?ex=676827e7&is=6766d667&hm=e2a9664ad073b7dd22391dde0ff46e6876b5b21cc33ba1aa9f033cd7ea9e8c14&=&width=1200&height=800",
+      websiteUrl:
+        "https://www.figma.com/design/017fjZWUhVM8Nugs5a22Ws/PiyushGoyal's-Assignment-Three?node-id=51-567&p=f&t=kNxygiKmyfplXpaw-0",
+      sourceCodeUrl: "https://github.com/username/design-system",
+      technologies: ["Mobile Design", "Figma"],
+      featured: true,
+    },
+  
+    // Others
+    {
+      id: 7,
+      title: `Motion Design for
+  About Us Page`,
+      description: `"I created a 1-minute motion design video for Yobee s
+      "About Us page, explaining its mission and services."`,
+      type: ["Others"],
+      image:
+        "https://media.discordapp.net/attachments/1156145270971715626/1320041449349185627/Frame_1000002484.png?ex=676827e6&is=6766d666&hm=81085425ce00b1816edc7664e2f51583faa681fb9bb9749367ba25d2dea18eec&=&width=1200&height=800",
+      websiteUrl: "https://youtu.be/1PuQBU_T4B8",
+      sourceCodeUrl: "https://github.com/username/design-dev-project",
+      technologies: ["Motion Design", "After Effects", "Figma"],
+      featured: true,
+    },
+    {
+      id: 8,
+      title: `Enhancing UX through
+      animations`,
+      description: `"I integrated micro-interactions and animated visuals
+      through Lottie animations throughout client's website
+      to enhance user experience and storytelling."`,
+      type: ["Others"],
+      image:
+        "https://media.discordapp.net/attachments/1156145270971715626/1320041450343235605/Frame_1000002487.png?ex=676827e6&is=6766d666&hm=24b32d5c5bc29adc7bc0730258d5438f5e47eb3627a987aa0ce807c0c3b50c44&=&width=1200&height=800",
+      websiteUrl: "https://yobee.co.in/publisher",
+      sourceCodeUrl: "https://github.com/username/design-system",
+      technologies: ["Motion Design", "After Effects", "Figma"],
+      featured: true,
+    },
+    {
+      id: 8,
+      title: "Announcement Motion Graphics",
+      description: `"I animated motion graphics for a rebranding announcement"`,
+      type: ["Others"],
+      image:
+        "https://media.discordapp.net/attachments/1156145270971715626/1320062370638205069/Frame_1000004535.png?ex=67683b62&is=6766e9e2&hm=aceeb1f77bf78fb3eef4d4c1b4a53c3931b7f6b7dc745ea6a088beffc67448ac&=&width=1200&height=800",
+      websiteUrl: "https://youtube.com/shorts/MNvswBrXung",
+      sourceCodeUrl: "https://github.com/username/design-dev-project",
+      technologies: ["Motion Design", "After Effects", "Figma"],
+      featured: true,
+    },
+    {
+      id: 9,
+      title: "Marketing Motion Graphics",
+      description: `"I animated motion graphics for socials to promote client's products"`,
+      type: ["Others"],
+      image:
+        "https://media.discordapp.net/attachments/1156145270971715626/1320041450033119242/Frame_1000002486.png?ex=676827e6&is=6766d666&hm=d5b6a84b43fcf0aedd677feb51f514d1dd07d47a924cdb4e9a8c0bac21064de4&=&width=1200&height=800",
+      websiteUrl: "https://youtube.com/shorts/GMYxY7NeECE",
+      sourceCodeUrl: "https://github.com/username/design-dev-project",
+      technologies: ["Motion Design", "After Effects", "Figma"],
+      featured: true,
+    },
+  ];
+  
