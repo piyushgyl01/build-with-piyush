e43da39f517dc7beadca7edc165b93aca07617c6
Piyush Goyal <piyush2022ug@gmail.com>
2025-01-20 20:07:59 +0530

    fix: data changes

--- src/Data.js
+++ src/Data.js
@@ -35,109 +35,109 @@ export const projects = [
     },
   
     // Product Design Projects
-    {
-      id: 4,
-      title: "Customer Support Page",
-    description: `"I designed a customer support and issue
-    tracking system to provide quick, clear answers to
-    users' product-related questions."`,
-    type: ["Product Design"],
-    image:
-      "https://github.com/piyushgyl01/images/blob/main/customerSupport.png?raw=true",
-    websiteUrl:
-      "https://www.figma.com/design/ex0UCTxbz3SNf0OkaeUG6I/Customer-Support-Page-By-Piyush-Goyal?node-id=0-1&t=ESdVthF52u2eR98C-1",
-    sourceCodeUrl: "https://github.com/username/design-system",
-    technologies: ["Web Design", "Figma"],
-    featured: true,
+    // {
+    //   id: 4,
+    //   title: "Customer Support Page",
+    // description: `"I designed a customer support and issue
+    // tracking system to provide quick, clear answers to
+    // users' product-related questions."`,
+    // type: ["Product Design"],
+    // image:
+    //   "https://github.com/piyushgyl01/images/blob/main/customerSupport.png?raw=true",
+    // websiteUrl:
+    //   "https://www.figma.com/design/ex0UCTxbz3SNf0OkaeUG6I/Customer-Support-Page-By-Piyush-Goyal?node-id=0-1&t=ESdVthF52u2eR98C-1",
+    // sourceCodeUrl: "https://github.com/username/design-system",
+    // technologies: ["Web Design", "Figma"],
+    // featured: true,
 
-    },
-    {
-      id: 5,
-      title: "Work Showcase Design",
-      description: `"I designed and animated Design Studios' work
-      showcase page, starting with an eye-catching hero
-      section to immediately grab visitors' attention."`,
-      type: ["Product Design"],
-      image:
-        "https://github.com/piyushgyl01/images/blob/main/showcase.png?raw=true",
-      websiteUrl:
-        "https://www.figma.com/design/dgubJXu8P1gKA8vu3fklpa/Blog-Page-by-Piyush-Goyal?node-id=0-1&t=ESdVthF52u2eR98C-1",
-      sourceCodeUrl: "https://github.com/username/design-system",
-      technologies: ["Web Design", "Figma"],
-      featured: true,
+    // },
+    // {
+    //   id: 5,
+    //   title: "Work Showcase Design",
+    //   description: `"I designed and animated Design Studios' work
+    //   showcase page, starting with an eye-catching hero
+    //   section to immediately grab visitors' attention."`,
+    //   type: ["Product Design"],
+    //   image:
+    //     "https://github.com/piyushgyl01/images/blob/main/showcase.png?raw=true",
+    //   websiteUrl:
+    //     "https://www.figma.com/design/dgubJXu8P1gKA8vu3fklpa/Blog-Page-by-Piyush-Goyal?node-id=0-1&t=ESdVthF52u2eR98C-1",
+    //   sourceCodeUrl: "https://github.com/username/design-system",
+    //   technologies: ["Web Design", "Figma"],
+    //   featured: true,
   
-    },
-    {
-      id: 6,
-      title: "Pocket Pod",
-      description: `"I redesigned PocketPod's user
-      onboarding flow to simplify AI podcast
-      generation."`,
-      type: ["Product Design"],
-      image:
-        "https://github.com/piyushgyl01/images-db/blob/main/pocketPod.png?raw=true",
-      websiteUrl:
-        "https://www.figma.com/design/017fjZWUhVM8Nugs5a22Ws/PiyushGoyal's-Assignment-Three?node-id=51-567&p=f&t=kNxygiKmyfplXpaw-0",
-      sourceCodeUrl: "https://github.com/username/design-system",
-      technologies: ["Mobile Design", "Figma"],
-      featured: true,
+    // },
+    // {
+    //   id: 6,
+    //   title: "Pocket Pod",
+    //   description: `"I redesigned PocketPod's user
+    //   onboarding flow to simplify AI podcast
+    //   generation."`,
+    //   type: ["Product Design"],
+    //   image:
+    //     "https://github.com/piyushgyl01/images-db/blob/main/pocketPod.png?raw=true",
+    //   websiteUrl:
+    //     "https://www.figma.com/design/017fjZWUhVM8Nugs5a22Ws/PiyushGoyal's-Assignment-Three?node-id=51-567&p=f&t=kNxygiKmyfplXpaw-0",
+    //   sourceCodeUrl: "https://github.com/username/design-system",
+    //   technologies: ["Mobile Design", "Figma"],
+    //   featured: true,
   
-    },
+    // },
   
     // Others
-    {
-      id: 7,
-      title: "Motion Design for About Us Page",
-    description: `"I created a 1-minute motion design video for Yobee s
-    "About Us page, explaining its mission and services."`,
-    type: ["Motion Design"],
-    image:
-      "https://github.com/piyushgyl01/images-db/blob/main/what.png?raw=true",
-    websiteUrl: "https://youtu.be/1PuQBU_T4B8",
-    sourceCodeUrl: "https://github.com/username/design-dev-project",
-    technologies: ["Motion Design", "After Effects", "Figma"],
-    featured: true,
+    // {
+    //   id: 7,
+    //   title: "Motion Design for About Us Page",
+    // description: `"I created a 1-minute motion design video for Yobee s
+    // "About Us page, explaining its mission and services."`,
+    // type: ["Motion Design"],
+    // image:
+    //   "https://github.com/piyushgyl01/images-db/blob/main/what.png?raw=true",
+    // websiteUrl: "https://youtu.be/1PuQBU_T4B8",
+    // sourceCodeUrl: "https://github.com/username/design-dev-project",
+    // technologies: ["Motion Design", "After Effects", "Figma"],
+    // featured: true,
 
-    },
-    {
-      id: 8,
-      title: "Enhancing UX through animations",
-    description: `"I integrated micro-interactions and animated visuals
-    through Lottie animations throughout client's website
-    to enhance user experience and storytelling."`,
-    type: ["Motion Design"],
-    image:
-      "https://github.com/piyushgyl01/images-db/blob/main/Frame%201000002487.png?raw=true",
-    websiteUrl: "https://yobee.co.in/publisher",
-    sourceCodeUrl: "https://github.com/username/design-system",
-    technologies: ["Motion Design", "After Effects", "Figma"],
-    featured: true,
+    // },
+    // {
+    //   id: 8,
+    //   title: "Enhancing UX through animations",
+    // description: `"I integrated micro-interactions and animated visuals
+    // through Lottie animations throughout client's website
+    // to enhance user experience and storytelling."`,
+    // type: ["Motion Design"],
+    // image:
+    //   "https://github.com/piyushgyl01/images-db/blob/main/Frame%201000002487.png?raw=true",
+    // websiteUrl: "https://yobee.co.in/publisher",
+    // sourceCodeUrl: "https://github.com/username/design-system",
+    // technologies: ["Motion Design", "After Effects", "Figma"],
+    // featured: true,
 
-    },
-    {
-      id: 9,
-      title: "Announcement Motion Graphics",
-    description: `"I animated motion graphics for a rebranding announcement"`,
-    type: ["Motion Design"],
-    image:
-      "https://github.com/piyushgyl01/images-db/blob/main/r.png?raw=true",
-    websiteUrl: "https://youtube.com/shorts/MNvswBrXung",
-    sourceCodeUrl: "https://github.com/username/design-dev-project",
-    technologies: ["Motion Design", "After Effects", "Figma"],
-    featured: true,
+    // },
+    // {
+    //   id: 9,
+    //   title: "Announcement Motion Graphics",
+    // description: `"I animated motion graphics for a rebranding announcement"`,
+    // type: ["Motion Design"],
+    // image:
+    //   "https://github.com/piyushgyl01/images-db/blob/main/r.png?raw=true",
+    // websiteUrl: "https://youtube.com/shorts/MNvswBrXung",
+    // sourceCodeUrl: "https://github.com/username/design-dev-project",
+    // technologies: ["Motion Design", "After Effects", "Figma"],
+    // featured: true,
 
-    },
-    {
-      id: 10,
-      title: "Marketing Motion Graphics - 2",
-    description: `"I animated motion graphics for socials to promote client's products"`,
-    type: ["Motion Design"],
-    image:
-      "https://github.com/piyushgyl01/images-db/blob/main/Frame%201000002486.png?raw=true",
-    websiteUrl: "https://www.youtube.com/shorts/0C99eO1643c",
-    sourceCodeUrl: "https://github.com/username/design-dev-project",
-    technologies: ["Motion Design", "After Effects", "Figma"],
-    featured: true,
+    // },
+    // {
+    //   id: 10,
+    //   title: "Marketing Motion Graphics - 2",
+    // description: `"I animated motion graphics for socials to promote client's products"`,
+    // type: ["Motion Design"],
+    // image:
+    //   "https://github.com/piyushgyl01/images-db/blob/main/Frame%201000002486.png?raw=true",
+    // websiteUrl: "https://www.youtube.com/shorts/0C99eO1643c",
+    // sourceCodeUrl: "https://github.com/username/design-dev-project",
+    // technologies: ["Motion Design", "After Effects", "Figma"],
+    // featured: true,
 
-    },
+    // },
   ];
